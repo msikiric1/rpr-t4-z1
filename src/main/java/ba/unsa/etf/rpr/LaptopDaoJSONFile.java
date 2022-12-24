afc8d03269288df4846775bbffc455a16e16f0ea
msikiric1 <msikiric1@etf.unsa.ba>
2022-12-24 21:24:29 +0100

    implement methods in LaptopDaoSerializableFile class

--- src/main/java/ba/unsa/etf/rpr/LaptopDaoJSONFile.java
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoJSONFile.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
-public class LaptopDaoJSONFile {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LaptopDaoJSONFile implements LaptopDao {
+    private File file;
+    private ArrayList<Laptop> laptopi;
+
+    @Override
+    public void dodajLaptopUListu(Laptop laptop) {
+
+    }
+
+    @Override
+    public void dodajLaptopUFile(Laptop laptop) {
+
+    }
+
+    @Override
+    public Laptop getLaptop(String procesor) {
+        return null;
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptopi) {
+
+    }
+
+    @Override
+    public List<Laptop> vratiPodatkeIzDatoteke() {
+        return null;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/LaptopDaoSerializableFile.java
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoSerializableFile.java
@@ -1,4 +1,55 @@
 package ba.unsa.etf.rpr;
 
-public class LaptopDaoSerializableFile {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LaptopDaoSerializableFile implements LaptopDao {
+    private File file;
+    private ArrayList<Laptop> laptopi;
+
+    public LaptopDaoSerializableFile(File file) {
+        this.file = new File("serializable.dat");
+    }
+
+    @Override
+    public void dodajLaptopUListu(Laptop laptop) {
+        laptopi.add(laptop);
+    }
+
+    @Override
+    public void dodajLaptopUFile(Laptop laptop) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+            oos.writeObject(laptop);
+            oos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Laptop getLaptop(String procesor) {
+        for(Laptop laptop : laptopi) {
+            if(laptop.getProcesor().equals(procesor))
+                return laptop;
+        }
+        return null;
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptopi) {
+        this.laptopi.addAll(laptopi);
+    }
+
+    @Override
+    public List<Laptop> vratiPodatkeIzDatoteke() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+            this.laptopi.addAll((ArrayList<Laptop>) ois.readObject());
+            ois.close();
+        } catch(ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
